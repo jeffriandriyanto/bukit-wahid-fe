d35c8a5e7d9518083b75840d635db45d5f620ab0
Jeffri Andriyanto <jeff.tm07@gmail.com>
2026-09-19 12:42:35 +0700

    Fix docs and bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      version: "2.0.13",
+      version: "2.0.14",
       siteUrl: 'https://rw11bukitwahid.com',
       baseUrl: process?.env?.NUXT_PUBLIC_BASE_URL || 'https://api.rw11bukitwahid.com/api/'
     },
